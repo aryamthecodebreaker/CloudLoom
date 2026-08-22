d7128fe7b3df481c143dd499abeed62341013d36
aryamthecodebreaker <aryamgoyal18@gmail.com>
2026-08-22 17:02:17 +0530

    chore: remove temp script

--- _ids.tmp.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-async function main() {
-  const d = new PrismaClient();
-  const rows = await d.issue.findMany({ where: { status: "OPEN" }, take: 3, orderBy: { refId: "asc" }, select: { id: true, refId: true } });
-  console.log(JSON.stringify(rows));
-  await d.$disconnect();
-}
-main();
